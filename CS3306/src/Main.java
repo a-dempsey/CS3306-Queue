fb46338207c1169798d22291f06d7adf1922ffaa
a-dempsey <amydempsey0@gmail.com>
2022-08-11 19:19:49 +0100

    main file to demonstrate functionality of queue class

---
+++ CS3306/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+        Queue queue = new Queue();
+        queue.size();
+        queue.add(12);
+        queue.element();
+        queue.peek();
+        queue.size();
+        queue.poll();
+        queue.size();
+    }
+}
